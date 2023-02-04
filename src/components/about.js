d4670158453d7b406d755eebfaf69c30631d6411
llleeeaaannn <piercefrank10@gmail.com>
2023-02-04 17:23:23 +0000

    Completed about section

--- src/components/about.js
+++ src/components/about.js
@@ -4,35 +4,36 @@ const About = () => {
   return (
     <div id="about">
       <section className="sectionone">
-        <div>
+        <div className="imageone">
           <img src="images/restaurant.jpg" alt="The restaurant"></img>
         </div>
 
-        <div class="textone">
+        <div className="textone">
           <p><strong>The Independent Pizza Company</strong> opened in the summer of 1984 and 2023 marks 39 years trading in Drumcondra. Founded by two Trinity College science graduates, Independent Pizza, as it's colloquially known, has become a staple of the Drumcondra food scene.</p>
 
-          <p>We offer seating for groups large and small and also provide a takeaway and deliver service.</p>
+          <p>We offer <strong>seating</strong> for groups large and small and also provide a <strong>takeaway</strong> and <strong>delivery</strong> service.</p>
         </div>
       </section>
 
       <section>
-        <div>
+        <div className="texttwo">
           <p>Our pizza style is American – light crisp bases with both traditional and contemporary topping combinations. We also have several non-pizza options including chicken wings, main course salads, roasted Irish half-chicken and more.</p>
 
-          <p>Our signature pizzas are baked in our Woodstone pizza oven which was imported for us from Washington State in the USA. These pizza ovens are regarded as the best in the world and we believe help us create outstanding pizza bases.</p>
+          <p>Our signature pizzas are baked in our <strong>Woodstone pizza oven</strong> which was imported for us from <strong>Washington State</strong> in the USA. These pizza ovens are regarded as the best in the world and we believe help us create outstanding pizza bases.</p>
         </div>
 
-        <div>
+        <div className="imagetwo">
+          <img src="images/pizzaphoto.jpeg" alt="Pizza on board"></img>
         </div>
       </section>
 
       <section>
-        <div>
+        <div className="imagethree">
+          <img src="images/oven.jpeg" alt="Pizza oven"></img>
         </div>
 
-        <div>
-          <p>Our pizza dough is freshly made every day, on the premises, using just five ingredients:
-          Flour, water, yeast and small amounts of salt and sugar.</p>
+        <div className="textthree">
+          <p>Our pizza dough is freshly made every day, on the premises, using just five ingredients: <strong> Flour, water, yeast, salt</strong> and <strong>sugar.</strong></p>
         </div>
       </section>
 
