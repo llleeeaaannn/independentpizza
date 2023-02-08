26d3a3eb78d3330d86bef38a18257543e1161fb8
llleeeaaannn <piercefrank10@gmail.com>
2023-02-08 15:28:31 +0000

    Moved map position in socials component

--- src/components/socials.js
+++ src/components/socials.js
@@ -2,11 +2,6 @@ const Socials = ({ style }) => {
 
   return (
     <div id="socials" className={style}>
-      <a href="https://goo.gl/maps/EFwraScp3MWDR8dN8" rel="noopener noreferrer" target="_blank">
-        <svg viewBox="0 0 256 256">
-          <path d="M128.1 16a88.1 88.1 0 0 0-88 88c0 75.3 80 132.2 83.4 134.6a8.3 8.3 0 0 0 9.2 0c3.4-2.4 83.4-59.3 83.4-134.6a88.1 88.1 0 0 0-88-88Zm0 56a32 32 0 1 1-32 32a32 32 0 0 1 32-32Z"/>
-        </svg>
-      </a>
 
       <a href="mailto:customercare@ilovepizza.ie" rel="noopener noreferrer" target="_blank">
         <svg viewBox="0 0 24 24">
@@ -25,6 +20,13 @@ const Socials = ({ style }) => {
           <path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"/>
         </svg>
       </a>
+
+      <a href="https://goo.gl/maps/EFwraScp3MWDR8dN8" rel="noopener noreferrer" target="_blank">
+        <svg viewBox="0 0 256 256">
+          <path d="M128.1 16a88.1 88.1 0 0 0-88 88c0 75.3 80 132.2 83.4 134.6a8.3 8.3 0 0 0 9.2 0c3.4-2.4 83.4-59.3 83.4-134.6a88.1 88.1 0 0 0-88-88Zm0 56a32 32 0 1 1-32 32a32 32 0 0 1 32-32Z"/>
+        </svg>
+      </a>
+      
     </div>
   )
 }
