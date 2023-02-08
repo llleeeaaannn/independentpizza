2ee686e3f9b6003d069c473577d8d0edda5b8492
llleeeaaannn <piercefrank10@gmail.com>
2023-02-08 15:26:48 +0000

    Added map to socials, refactored Menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -8,7 +8,6 @@ const Menu = () => {
     <div id="menu">
       <nav>
         <Logo />
-        <Socials />
         <Close />
       </nav>
       <div className="content">
@@ -26,11 +25,14 @@ const Menu = () => {
             <span>28 Drumcondra Rd Lower</span>
             <span>Drumcondra, Dublin 9</span>
             <span>D09 X034</span>
-            <a href="https://goo.gl/maps/EFwraScp3MWDR8dN8" rel="noopener noreferrer" target="_blank">
-              <svg viewBox="0 0 256 256">
-                <path d="M128.1 16a88.1 88.1 0 0 0-88 88c0 75.3 80 132.2 83.4 134.6a8.3 8.3 0 0 0 9.2 0c3.4-2.4 83.4-59.3 83.4-134.6a88.1 88.1 0 0 0-88-88Zm0 56a32 32 0 1 1-32 32a32 32 0 0 1 32-32Z"/>
-              </svg>
-            </a>
+            <div>
+              <Socials />
+              <a href="https://goo.gl/maps/EFwraScp3MWDR8dN8" rel="noopener noreferrer" target="_blank">
+                <svg viewBox="0 0 256 256">
+                  <path d="M128.1 16a88.1 88.1 0 0 0-88 88c0 75.3 80 132.2 83.4 134.6a8.3 8.3 0 0 0 9.2 0c3.4-2.4 83.4-59.3 83.4-134.6a88.1 88.1 0 0 0-88-88Zm0 56a32 32 0 1 1-32 32a32 32 0 0 1 32-32Z"/>
+                </svg>
+              </a>
+            </div>
           </div>
         </div>
       </div>
