93b0874cb8a88ba0aa5c6d373941ff50499501ad
llleeeaaannn <piercefrank10@gmail.com>
2023-02-09 14:09:34 +0000

    Added links to FAQ answers and wrote more copy

--- src/views/faq.js
+++ src/views/faq.js
@@ -13,14 +13,18 @@ const Faq = () => {
   const q1 = <span>What type of cuisine do we serve?</span>
   const a1 = <p>Our menu consists primarily of pizza but we also serve options including burgers and salads. Our pizzas are also available with a gluten-free base. Of course, seasonal starters and desserts are served year round too. You can find our menu <Link to="/menu">here</Link></p>
 
-  const q2 = <span></span>
-  const a2 = <p></p>
+  const q2 = <span>What are our opening hours?</span>
+  const a2 = <div>
+                <span>Monday & Tuesday: Closed</span><br/>
+                <span>Wednesday & Thursday: 16:00 - 21:00</span><br/>
+                <span>Friday & Saturday: 12:00 - 22:00</span><br/>
+                <span>Sunday: 12:00 - 21:00</span>
+              </div>
+  const q3 = <span>Do we have vouchers?</span>
+  const a3 = <p>We do! Vouchers are available in store or you can buy them <Link to="/reservations">here.</Link></p>
 
-  const q3 = <span></span>
-  const a3 = <p></p>
-
-  const q4 = <span></span>
-  const a4 = <p></p>
+  const q4 = <span>Can you come to us with your dog?</span>
+  const a4 = <p>Unfortunately, pets aren't allowed in our restaurant.</p>
 
   const q5 = <span></span>
   const a5 = <p></p>
@@ -33,9 +37,11 @@ const Faq = () => {
 
           <Question questionText={q1} answerText={a1} />
 
-          <Question questionText={q1} answerText={a1} />
+          <Question questionText={q2} answerText={a2} />
 
-          <Question questionText={q1} answerText={a1} />
+          <Question questionText={q3} answerText={a3} />
+
+          <Question questionText={q4} answerText={a4} />
         </div>
       </div>
       { menu && <Menu /> }
