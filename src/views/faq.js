9b1c510243a798d932f85e937db8193c8dd048f4
llleeeaaannn <piercefrank10@gmail.com>
2023-02-09 12:51:32 +0000

    Refactored all FAQ logic into question component

--- src/views/faq.js
+++ src/views/faq.js
@@ -3,58 +3,22 @@ import Navbar from '../components/navbar';
 import { useState, useContext } from 'react';
 import { AppContext } from '../App';
 import AnimateHeight from 'react-animate-height';
+import Question from '../components/question';
 
 const Faq = () => {
 
   const { menu } = useContext(AppContext);
-
-  const closed = 0;
-  const open = 'auto';
-  const [height1, setHeight1] = useState(closed);
-  const [height2, setHeight2] = useState(closed);
-  const [height3, setHeight3] = useState(closed);
-  const [height4, setHeight4] = useState(closed);
-  const [height5, setHeight5] = useState(closed);
-  const [height6, setHeight6] = useState(closed);
-  const [height7, setHeight7] = useState(closed);
 
   return (
     <>
       <Navbar />
       <div id="faq">
         <div className="inner">
-          <div className="f">
-            <div className="q" onClick={() => setHeight1(height1 === closed ? open : closed)}>
-              <h2>This is the question, This is the question, This is the questionThis is the question</h2>
-              <svg className={height1 ? "open" : ""} viewBox="0 0 72 72">
-                <path d="m17.5 17.5l37 37m0-37l-37 37"/>
-              </svg>
-            </div>
+          <Question questionText="This is the questionnnn" answerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." />
 
-            <AnimateHeight className="a" height={height1} duration={500}>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-            </AnimateHeight>
-          </div>
+          <Question questionText="This is the second questionnnn" answerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." />
 
-          <div className="f">
-            <div className="q" onClick={() => setHeight2(height2 === closed ? open : closed)}>
-              <h2>This is the question</h2>
-            </div>
-
-            <AnimateHeight className="a" height={height2} duration={500}>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-            </AnimateHeight>
-          </div>
-
-          <div className="f">
-            <div className="q" onClick={() => setHeight3(height3 === closed ? open : closed)}>
-              <h2>This is the question</h2>
-            </div>
-
-            <AnimateHeight className="a" height={height3} duration={500}>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-            </AnimateHeight>
-          </div>
+          <Question questionText="This is the lo lolo nvjusfbjb fweruuhvbweuh sjhgugeu fei" answerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500st of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s t of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." />
         </div>
       </div>
       { menu && <Menu /> }
