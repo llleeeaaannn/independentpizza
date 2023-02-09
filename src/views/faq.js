c70ed5a3043d7b4aca302893f7b1dc0c23d631a1
llleeeaaannn <piercefrank10@gmail.com>
2023-02-09 13:41:13 +0000

    Created data directory with faq data file

--- src/views/faq.js
+++ src/views/faq.js
@@ -4,6 +4,7 @@ import { useState, useContext } from 'react';
 import { AppContext } from '../App';
 import AnimateHeight from 'react-animate-height';
 import Question from '../components/question';
+import faqData from '../data/faqData';
 
 const Faq = () => {
 
@@ -16,7 +17,7 @@ const Faq = () => {
         <div className="inner">
           <Question questionText="This is the questionnnn" answerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." />
 
-          <Question questionText="This is the second questionnnn" answerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." />
+          <Question questionText={<>faqData[0].q</>} answerText={<>What type of <strong>cuisine</strong> do we serve?</>} />
 
           <Question questionText="This is the lo lolo nvjusfbjb fweruuhvbweuh sjhgugeu fei" answerText="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500st of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s t of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." />
         </div>
