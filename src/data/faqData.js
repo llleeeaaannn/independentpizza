f752b3d5fc107d5f6e1a9df6893b967a78884755
llleeeaaannn <piercefrank10@gmail.com>
2023-02-09 13:42:01 +0000

    Removed data directory

--- src/data/faqData.js
+++
@@ -1,8 +0,0 @@
-const faqData = [
-  {
-    q: "What type of <strong>cuisine</strong> do we serve?",
-    a: "Our menu consists primarily of pizza but we also serve options including burgers and salads. Our pizzas are also available with a gluten-free base. Of course, seasonal starters and desserts are served year round too."
-  }
-]
-
-export default faqData;
