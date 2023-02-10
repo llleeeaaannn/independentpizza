2022f03101a0b394c8691a5f28e3dbf1f340b025
llleeeaaannn <piercefrank10@gmail.com>
2023-02-10 17:16:21 +0000

    Began content for contact page, added map

--- src/views/contact.js
+++ src/views/contact.js
@@ -11,6 +11,11 @@ const Contact = () => {
     <>
       <Navbar />
       <div id="contact">
+        <div className="map">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d550.8354499112014!2d-6.260538939696275!3d53.36368798043172!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1aab0ff98e9a56d7!2sIndependent%20Pizza%20Company!5e0!3m2!1sen!2sie!4v1676049167791!5m2!1sen!2sie" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div className="details">
+        </div>
       </div>
       { menu && <Menu /> }
     </>
