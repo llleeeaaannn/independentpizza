92099589ca694dbc46fb2fabbd57e44a684cdfbf
llleeeaaannn <piercefrank10@gmail.com>
2023-02-11 11:42:26 +0000

    Refactored contact page content

--- src/views/contact.js
+++ src/views/contact.js
@@ -12,15 +12,17 @@ const Contact = () => {
     <>
       <Navbar />
       <div id="contact">
-        <div className="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d550.8354499112014!2d-6.260538939696275!3d53.36368798043172!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1aab0ff98e9a56d7!2sIndependent%20Pizza%20Company!5e0!3m2!1sen!2sie!4v1676049167791!5m2!1sen!2sie" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-        <div className="details">
-          <h2>Contact</h2>
-          <span>(01) 830 2044</span>
-          <span>28 Drumcondra Rd Lower, Dublin 9</span>
-          <Times />
-        </div>
+        <header><h2>Contact</h2></header>
+        <main>
+          <div className="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d550.8354499112014!2d-6.260538939696275!3d53.36368798043172!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x1aab0ff98e9a56d7!2sIndependent%20Pizza%20Company!5e0!3m2!1sen!2sie!4v1676049167791!5m2!1sen!2sie" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+          <div className="details">
+            <span>(01) 830 2044</span>
+            <span>28 Drumcondra Rd Lower, Dublin 9</span>
+            <Times />
+          </div>
+        </main>
       </div>
       { menu && <Menu /> }
     </>
